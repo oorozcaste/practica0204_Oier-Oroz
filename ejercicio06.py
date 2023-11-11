463bd75bccfd68003ae4ad4d3674f5534cb722a0
oorozcaste <145026396+oorozcaste@users.noreply.github.com>
2023-11-11 23:26:10 +0100

    Update ejercicio06.py

--- ejercicio06.py
+++ ejercicio06.py
@@ -1,16 +1,35 @@
-costos_fijos = 300000  # euros al año
-capacidad_maxima_negocios = 1500000  # euros
-costos_variables_proporcion = 2 / 3  # dos tercios
+ikasleak={}
+respuesta = input("que opcion vas a elegir (1) Añadir alumno/a, (2) Eliminar alumno/a, (3)"
+                  " Mostrar alumno/a, (4) Listar todo el alumnado, (5) Listar alumnado aprobado, (6) Terminar.")
+while True:
+     if respuesta == "1":
+         izena = input('nombre del alumno ')
+         abizenak= input('apellidos del alumno: ')
+         zembakia = input('teléfono del añumno: ')
+         posta = input('correo electrónico del alumno: ')
+         aprobado = input('¿ha aprobado el modulo?(si/no): ')
+         dni = input('Introduce DNI del alumno: ')
 
-# Calcular costos variables
-costos_variables = capacidad_maxima_negocios * costos_variables_proporcion
-
-# Calcular precio de venta por unidad (que en este caso es igual a la cifra de negocios máxima)
-precio_venta_por_unidad = capacidad_maxima_negocios
-
-print(precio_venta_por_unidad)
-# Calcular punto muerto
-punto_muerto = costos_fijos / (precio_venta_por_unidad - costos_variables)
-
-# Mostrar resultado
-print(f"El Punto Muerto es de {round(punto_muerto, 2)} euros.")
+         ikaslea = {"izena":izena,"abizenak":abizenak,"zembakia":zembakia,"posta":posta,"aprobado":aprobado}
+         ikasleak[dni] = ikaslea
+         respuesta = input("que opcion vas a elegir (1) Añadir alumno/a, (2) Eliminar alumno/a, (3)"
+                  " Mostrar alumno/a, (4) Listar todo el alumnado, (5) Listar alumnado aprobado, (6) Terminar.")
+     elif respuesta == "2":
+         borrado = input("dime el dni del alumno que desees borrar del classroom: ")
+         if borrado in ikasleak:
+             del ikasleak[borrado]
+     elif respuesta =="3":
+         datos = input("dime el dni del alumno sobre el que queiras saber: ")
+         if datos in ikasleak:
+             print(ikasleak[datos])
+     elif respuesta == "4":
+         for a, b in ikasleak.items():
+             print(a,b[0])
+     elif respuesta == "5":
+         for z in ikasleak.values():
+             if z == "si":
+                 print()
+    
+     elif respuesta == "terminar":
+         print ("gracias por usar nuestro programa") 
+         break
